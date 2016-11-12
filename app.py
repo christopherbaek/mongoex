5acf4a354549814729dd4be13583ffab08075b43
Christopher Baek <baekc@amazon.com>
2016-11-12 12:38:25 -0800

    Integrate Mongo query with Flask application

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import jsonify
+from pymongo import MongoClient
+
+# the Flask application
 app = Flask(__name__)
+
+# Mongo DB
+client = MongoClient('mongodb://localhost:27017/')
+db = client.myRetail
+
 
 @app.route('/')
 def index():
-	return "Hello World!"
+	# run Mongo query
+	products = [p for p in db.products.find({"product.item.fulfillment.is_po_box_prohibited": True})]
+	response_body = {'products': products}
+	return jsonify(**response_body)
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
