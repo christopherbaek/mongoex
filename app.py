03cb45dca84983fc553a13dba7aff86c79094962
a584839 <brandon.spruth@target.com>
2016-11-12 23:11:37 -0600

    added dbs func and templates

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ from pymongo import MongoClient
 # the Flask application
 app = Flask(__name__)
 
-# Mongo DB
-client = MongoClient('mongodb://localhost:27017/')
-db = client.myRetail
+app.config['MONGO_DBNAME'] = 'myRetail'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/myRetail'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/products')
+def products():
 	# run Mongo query
-	products = [p for p in db.products.find({"product.item.fulfillment.is_po_box_prohibited": True})]
+	products = [p for p in mongo.db.products.find({"product.item.fulfillment.is_po_box_prohibited": True})]
 	response_body = {'products': products}
 	return jsonify(**response_body)
 
--- db.py
+++ db.py
@@ -6,17 +6,22 @@ from pymongo import errors
 from bson import json_util
 from bson import ObjectId
 
-
+MONGO_URI = 'mongodb://localhost:27017/'
 url = 'http://redsky.target.com/v1/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics'
 
 
 def get_db():
     try:
-        client = MongoClient('mongodb://localhost:27017/')
+        client = MongoClient(MONGO_URI + 'myRetail')
         db = client.myRetail
         return db
     except errors.ConnectionFailure, e:
-        print "Could not connect to MongoDB: %s" % e
+        print "Could not connect to MongoDB: {}".format(e)
+
+
+def list_dbs():
+    client = MongoClient(MONGO_URI)
+    return client.database_names()
 
 
 def add_products(db):
@@ -24,13 +29,16 @@ def add_products(db):
         response = requests.get(url)
         obj = json.loads(response.content)
         db.products.insert(obj)
-
     except requests.exceptions.RequestException as e:
         print("Error: Invalid URL{}".format(e))
     except errors.DuplicateKeyError as e:
         print("duplicate Values in database: {}".format(e))
     except ValueError as e:
         print("Database insert error: {}".format(e))
+
+
+def delete_products(db):
+    db.products.drop()
 
 
 def get_raw_products(db):
@@ -40,29 +48,28 @@ def get_raw_products(db):
 
 
 def get_ascii_products(db):
-    obj = db.products.find().limit(1)
+    obj = db.products.find()
     products = list(obj)
     #json serializing mongoddb
     rs = json.dumps(products, default=json_util.default)
     return rs
 
 
-def get_one_product(db):
+def get_product_id(db):
     # list comprehension
-    products = [p for p in db.products.find({"product.item.fulfillment.is_po_box_prohibited": True})]
-    print(products)
+    #products = [p for p in db.products.find({"product.available_to_promise_network.product_id": 13860428})]
+    product_id = [p for p in db.products.find({"product.item.tcin": "13860428"})]
+    for p in db.products.find({"product.item.tcin": "13860428"}):
+        print p
+    return product_id
 
-    for product in db.products.find({"product.item.product_id:" "13860428"}):
-        print product
-
-def delete_products(db):
-    db.products.drop()
 
 if __name__ == "__main__":
     db = get_db()
     #delete_products(db)
     #add_products(db)
+    print(list_dbs())
     #print(get_ascii_products(db))
     #print(get_raw_products(db))
-    get_one_product(db)
+    get_product_id(db)
 
